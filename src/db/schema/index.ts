52ad07ffdfb522a11bc861dd85da0294a299c81b
daviduartedev <daviduarte.dev@gmail.com>
2025-07-13 14:02:20 -0300

    configurando o db

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,8 +1,5 @@
-import { pgTable, text, uuid, timestamp } from "drizzle-orm/pg-core";
+import { rooms } from "./rooms";
 
-export const rooms = pgTable('rooms', {
-    id: uuid('id').primaryKey().defaultRandom(),
-    namet: text('namet').notNull(),
-    description: text('description'),
-    createdAt: timestamp('created_at').defaultNow().notNull()
-})
+export const schema = {
+    rooms,
+}
